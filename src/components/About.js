b6af9534b61aec32ce01a40cd4a6b4275a3fdaa0
Julia Arrington <jarringt@uccs.edu>
2022-04-14 18:22:44 -0600

    setup, added about and interests component

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>I am a front-end developer. I like learning new technologies that make development more efficient.</p>
+        </div>
+    )
+}
---
+++ src/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <div>
+            <h2>Interests</h2>
+            <p>I love to create in many different mediums. When I'm not creating new websites, I like to knit, draw, and cook. I also have two cats that I love.</p>
+        </div>
+    )
+}
