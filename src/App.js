0ee9d84fb1853c6f2f1d111f1d00cee1a797ddbb
Julia Arrington <jarringt@uccs.edu>
2022-04-22 17:29:20 -0600

    added pfp and changed colors

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react"
 import About from "./components/About"
 import Interests from "./components/Interests"
 import Footer from "./components/Footer"
+import Info from "./components/Info"
 import "./style.css";
 
 
 export default function App() {
     return (
         <div className="container">
+            <Info />
             <About />
            <Interests />
-           <Footer />
+           <Footer /> 
         </div>
     )
 }
---
+++ src/components/Info.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Profile from "../profile-pic.png";
+
+export default function Main() {
+    return (
+        <div className="info">
+            <img src={Profile} alt="Anime style drawing of Julia Arrington"></img>
+        </div>
+    )
+}
