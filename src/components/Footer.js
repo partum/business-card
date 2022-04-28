73cf09defc98e4a6fdd183a2c46d78c885658cbc
Julia Arrington <jarringt@uccs.edu>
2022-04-27 19:06:47 -0600

    finalized project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Main() {
             <a href="https://www.linkedin.com/in/julia-arrington" title="View my LinkedIn page.">
                 <img src={LinkedInIcon} alt="LinkedIn logo."></img>
             </a>
-            <a href="https://github.com/partum" title="View my GitHub account">
+            <a href="https://github.com/partum" title="View my GitHub account.">
                 <img src={GitHubIcon} alt="GitHub logo."></img>
             </a>
         </div>
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,19 @@
 import React from "react"
 import Profile from "../profile-pic.png";
+import Mail from "../Mail.svg";
 
 export default function Main() {
     return (
         <div className="info">
             <img src={Profile} alt="Anime style drawing of Julia Arrington"></img>
+            <div>
+                <h1>Julia Arrington</h1>
+                <h2>Jr. Front-End Developer</h2>
+                <a href="partum.github.io" title="Go to Julia's portfolio">partum.github.io</a>
+            </div>
+            <a id="button" href="mailto:jarring.dev@gmail.com">
+                <img src={Mail} alt=""></img>
+                Email</a>
         </div>
     )
 }
